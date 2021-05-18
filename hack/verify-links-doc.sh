830f7dfdd8dc1b39aad561033b79c77d4845f699
Ran <huangran@pingcap.com>
2021-05-18 10:56:17 +0800

    merge 2 jobs; fix verify-links-doc.sh
    Signed-off-by: Ran <huangran@pingcap.com>
    
    fix script
    
    Signed-off-by: Ran <huangran@pingcap.com>
    
    add markdown-link-check.tpl
    
    Signed-off-by: Ran <huangran@pingcap.com>
    
    fix replacementpatterns
    
    Signed-off-by: Ran <huangran@pingcap.com>
    
    fix a single quote in filename
    
    Signed-off-by: Ran <huangran@pingcap.com>
    
    fix wrong replacementpatterns
    
    Signed-off-by: Ran <huangran@pingcap.com>
    
    test verify-links.sh
    
    Signed-off-by: Ran <huangran@pingcap.com>
    
    fix again script
    
    Signed-off-by: Ran <huangran@pingcap.com>

--- hack/verify-links-doc.sh
+++ hack/verify-links-doc.sh
@@ -7,7 +7,7 @@
 ROOT=$(unset CDPATH && cd $(dirname "${BASH_SOURCE[0]}")/.. && pwd)
 cd $ROOT
 
-npm install markdown-link-check@3.8.1
+npm install -g markdown-link-check@3.8.1
 
 VERBOSE=${VERBOSE:-}
 CONFIG_TMP=$(mktemp)
@@ -16,29 +16,27 @@ ERROR_REPORT=$(mktemp)
 trap 'rm -f $CONFIG_TMP $ERROR_REPORT' EXIT
 
 # Check all directories starting with 'v\d.*' and dev.
-for d in zh en; do
-    echo "info: checking links under $ROOT/$d directory..."
-    sed \
-        -e "s#<ROOT>#$ROOT#g" \
-        $ROOT/hack/markdown-link-check.tpl > $CONFIG_TMP
-    if [ -n "$VERBOSE" ]; then
-        cat $CONFIG_TMP
-    fi
-    while read -r tasks; do
-        for task in $tasks; do
-            (
-                output=$(npx markdown-link-check --config "$CONFIG_TMP" "$task" -q)
-                if [ $? -ne 0 ]; then
-                    printf "$output" >> $ERROR_REPORT
-                fi
-                if [ -n "$VERBOSE" ]; then
-                    echo "$output"
-                fi
-            ) &
-        done
-        wait
-    done <<<"$(find "$ROOT/$d" -type f -not -path './node_modules/*' -name '*.md' | xargs -n 10)"
-done
+echo "info: checking links under $ROOT directory..."
+sed \
+    -e "s#<ROOT>#$ROOT#g" \
+    $ROOT/hack/markdown-link-check.tpl > $CONFIG_TMP
+if [ -n "$VERBOSE" ]; then
+    cat $CONFIG_TMP
+fi
+while read -r tasks; do
+    for task in $tasks; do
+        (
+            output=$(markdown-link-check --config "$CONFIG_TMP" "$task" -q)
+            if [ $? -ne 0 ]; then
+                printf "$output" >> $ERROR_REPORT
+            fi
+            if [ -n "$VERBOSE" ]; then
+                echo "$output"
+            fi
+        ) &
+    done
+    wait
+done <<<"$(find "." -type f -not -path './node_modules/*' -name '*.md' | xargs -n 10)"
 
 error_files=$(cat $ERROR_REPORT | grep 'FILE: ' | wc -l)
 error_output=$(cat $ERROR_REPORT)
