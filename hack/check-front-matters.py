bc7876f2916486fb57703377f52d8cb6457ce0c2
Ran <huangran@pingcap.com>
2021-10-20 15:12:08 +0800

    chore: add check for frontmatter format (#722)
    * chore: add check for frontmatter format
    
    Signed-off-by: Ran <huangran@pingcap.com>
    
    * chore: add check for frontmatter format
    
    Signed-off-by: Ran <huangran@pingcap.com>
    
    * fix package name
    
    * prettify output
    
    * Update test-file.md
    
    * Delete test-file.md

--- hack/check-front-matters.py
+++ hack/check-front-matters.py
@@ -1,78 +1,53 @@
-import re
+from datetime import date
 import sys
 import os
-from datetime import date
+import frontmatter
 
-def check_front_matter(filename):
 
-    frontMatter = []
-    common = ['title', 'author', 'date', 'summary', 'tags', 'image']
-    blog = ['categories']
-    caseStudy = ['customer', 'customerCategory', 'logo']
-    # optional = ['press_release']
-    missing = []
+def metadata_is_complete(metadata, filename):
+    """Check whether the metadata contains every item we need."""
+
+    #print(filename)
+
+    # don't check blogs before 2019-11-29
+    if metadata['date'] < date(2019,11,29):
+        return 0
+
+    meta = set(metadata.keys())
+    # common = ['title', 'author', 'date', 'summary', 'tags', 'image']
+    blog = {'categories', 'title', 'author', 'date', 'summary', 'tags', 'image'}
+    case_study = {'customer', 'customerCategory', 'logo', 'title', 'author', 'date', 'summary', 'tags', 'image'}
+
+    # check if metadata keys are complete
+    if blog <= meta or case_study <= meta:
+        return 0
+    else:
+        print("\n" + filename + " misses frontmatters. Please check again.\n")
+        return 1
+
+
+def metadata_is_correct(metadata, filename):
+    """Check whehter the metadata conforms to our requirements."""
+    #print(filename)
 
     flag = 0
-    type = 0
 
+    if ('categories' in metadata.keys()) and (str(metadata['categories'])[2:-2] not in
+    ('Product', 'Engineering', 'Community', 'Company')):
+        print(filename + " has an unusual category: " + str(metadata['categories']) +
+        ". Please verify its correctness. \n")
+        flag += 1
 
-    with open(filename,'r') as file:
+    if ('image' in metadata.keys()) and (metadata['image'][:13] != '/images/blog/'):
+        print(filename + " has a wrong image path. It should begin with '/images/blog/'.\n")
+        flag += 1
 
-        dashes = 0
-        word = ''
-        publish_date = date.today()
-        banner_date = date(2019,11,29) # blogs before this date don't have banner
+    #if len(metadata['title']) > 60:
+    #    print(filename + " has a long title (< 60 characters recommended). Are you sure?\n")
+    #if len(metadata['summary']) > 300:
+    #    print(filename + " has a long summary (< 300 characters recommended). Are you sure?\n")
 
-        for line in file:
-            if re.match(r'(-){3}', line) and dashes == 0: #front matter begins
-                dashes = 1
-            elif re.match(r'(-){3}', line) and dashes == 1: #front matter ends
-                break
-            else:
-                word = line[:line.find(':')]
-                if word == 'date':
-                    publish_date = date.fromisoformat(line[line.find(':')+2:-1])
-                    if publish_date < banner_date:
-                        return 0 # don't check blogs before 2019-11-29
-                frontMatter.append(word)
-
-        for item in common:
-            if item in frontMatter:
-                continue
-            else:
-                flag += 1
-                missing.append(item)
-
-        if 'customer' in frontMatter or 'customerCategory' in frontMatter or 'logo' in frontMatter:
-            type = 1
-            for item in caseStudy:
-                if item in frontMatter:
-                    continue
-                else:
-                    flag += 1
-                    missing.append(item)
-        elif 'categories' in frontMatter:
-            type = 2
-            for item in blog:
-                if item in frontMatter:
-                    continue
-                else:
-                    flag += 1
-                    missing.append(item)
-        else:
-            flag += 1
-
-    if flag:
-        if type == 1:
-            print("\n" + filename + " is a case study, and it misses the following frontmatters:\n")
-        elif type == 2:
-            print("\n" + filename + " is a blog, and it misses the following frontmatters:\n")
-        else:
-            print("\n" + filename + " is neither blog nor case study. Please make sure you add 'categories' or 'customer'.")
-            if flag > 1:
-                print("In addtion, it misses the following frontmatters:\n")
-        print(missing)
-        return 1
+    return flag
 
 
 if __name__ == "__main__":
@@ -80,11 +55,20 @@ if __name__ == "__main__":
     count = 0
 
     for filename in sys.argv[1:]:
-        if os.path.isfile(filename):
-            mark = check_front_matter(filename)
+        mark = 0
+
+        if os.path.isfile(filename) and filename not in ('README.md','pull-request-template.md'):
+            #metadata = {}
+            #content = {}
+            with open(filename) as f:
+                metadata, content = frontmatter.parse(f.read())
+            if metadata_is_complete(metadata, filename):
+                mark += 1
+            if metadata_is_correct(metadata, filename):
+                mark += 1
             if mark:
-                count+=1
+                count += 1
 
     if count:
-        print("\nThe above issues will cause website build failure. Please fix them.")
+        print("The above issues will cause website build failure. Please fix them.\n")
         exit(1)
